75f095fa364158e30053cd31247d6381d2e45712
manonja <manon.jacquin@gmail.com>
2024-06-13 15:58:52 +0200

    feat: uodate subscribe link

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -63,7 +63,7 @@ const siteConfig: SiteConfig = {
         },
         {
             text: 'Subscribe',
-            href: 'https://mirth-and-mastery.beehiiv.com/subscribe'
+            href: 'https://mirth-and-mastery.beehiiv.com'
         },
         // {
         //     text: 'Writing',

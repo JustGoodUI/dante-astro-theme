a5778a985f168b6f2c874afbb1711ad720eb0d1c
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-07-25 11:59:27 +0530

    cleanup

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -40,10 +40,10 @@ export type SiteConfig = {
 const siteConfig: SiteConfig = {
     title: 'sagnikc395.xyz',
     subtitle: 'my personal website on internet',
-    description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
+    description: '',
     image: {
-        src: '/dante-preview.jpg',
-        alt: 'Dante - Astro.js and Tailwind CSS theme'
+        src: '',
+        alt: ''
     },
     headerNavLinks: [
         {
@@ -88,11 +88,11 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
+        title: `thoughts on programming, mathematical logic and abstractions! it's gonna be a fun ride 🫶`,
         text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
         image: {
-            src: '/hero.jpeg',
-            alt: 'A person sitting at a desk in front of a computer'
+            src: '/hero.jpg',
+            alt: 'a oil painting of countryside'
         },
         actions: [
             {

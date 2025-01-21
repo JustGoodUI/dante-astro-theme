aeff95e2b461e4cf85b628ba573cbf384da48086
JuanSJaen <juansaezjaen@gmail.com>
2025-01-21 18:08:15 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://JuanSJaen.githuub.io',
     integrations: [
         mdx(),
         sitemap(),

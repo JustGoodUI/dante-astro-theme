d794757b78abda22c11103441dcf477af02afb85
JuanSJaen <juansaezjaen@gmail.com>
2025-01-21 18:15:07 +0100

    Pagina Principal

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -38,12 +38,12 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
-    description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
+    title: 'JSJ',
+    subtitle: 'Juan CV y Blog',
+    description: 'CV y Blog de Juan S.',
     image: {
-        src: '/dante-preview.jpg',
-        alt: 'Dante - Astro.js and Tailwind CSS theme'
+        src: '/JSJ.jpg',
+        alt: 'Mi foto'
     },
     headerNavLinks: [
         {
@@ -82,25 +82,22 @@ const siteConfig: SiteConfig = {
         }
     ],
     socialLinks: [
-        {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
-        },
+        
         {
             text: 'Instagram',
-            href: 'https://instagram.com/'
+            href: 'https://www.instagram.com/juansaezj/'
         },
         {
-            text: 'X/Twitter',
-            href: 'https://twitter.com/'
+            text: 'Linkedin',
+            href: 'https://www.linkedin.com/in/juan-s%C3%A1ez-ja%C3%A9n-4b8456168/'
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        title: 'Hi There & Welcome my Web!',
+        text: "I'm Juan Sáez, an engineering student specializing in electronics. I am primarily interested in the world of automation and am currently doing an internship at Siemens, working with industrial computers",
         image: {
-            src: '/hero.jpeg',
-            alt: 'A person sitting at a desk in front of a computer'
+            src: '/JSJ.jpg',
+            alt: 'Yo'
         },
         actions: [
             {
@@ -110,8 +107,8 @@ const siteConfig: SiteConfig = {
         ]
     },
     subscribe: {
-        title: 'Subscribe to Dante Newsletter',
-        text: 'One update per week. All the latest posts directly in your inbox.',
+        title: 'Subscribe to Juan Newsletter',
+        text: 'All the latest posts directly in your inbox.',
         formUrl: '#'
     },
     postsPerPage: 8,

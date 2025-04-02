11ab9fd5fc47462c111fa0d12aba7e37b076f7a4
Valder Maquera <58677209+Soyvalder@users.noreply.github.com>
2025-04-02 18:12:08 -0500

    Update site info

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -40,7 +40,7 @@ export type SiteConfig = {
 
 const siteConfig: SiteConfig = {
     website: 'https://example.com',
-    title: 'Dante',
+    title: 'Valerio',
     subtitle: 'Minimal Astro.js theme',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
@@ -60,11 +60,7 @@ const siteConfig: SiteConfig = {
             text: 'Blog',
             href: '/blog'
         },
-        {
-            text: 'Tags',
-            href: '/tags'
-        }
-    ],
+        ],
     footerNavLinks: [
         {
             text: 'About',
@@ -78,28 +74,21 @@ const siteConfig: SiteConfig = {
             text: 'Terms',
             href: '/terms'
         },
-        {
-            text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
-        }
+        
     ],
-    socialLinks: [
+    socialLinks: [       
         {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
-        },
-        {
-            text: 'Instagram',
-            href: 'https://instagram.com/'
-        },
+            text: 'linkedIn',
+            href: 'https://www.linkedin.com/in/analyts-vm//'
+        }
         {
             text: 'X/Twitter',
             href: 'https://twitter.com/'
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        title: 'Hola bienvenido!',
+        text: "Soy **Valerio Maquera**, Analista de datos, dedicado a la colaboración y la inteligencia artificial. Mi enfoque se basa en las finanzas, blockchain. Tengo un profundo aprecio por los datos y el software, Siéntete libre de explorar algunos de mis proyectos de codificación en<a href='https://github.com/Soyvalder?tab=repositories'>GitHub</a> or follow me on <a href='https://x.com/valderdata'>Twitter/X</a>.",
         image: {
             src: '/hero.jpeg',
             alt: 'A person sitting at a desk in front of a computer'

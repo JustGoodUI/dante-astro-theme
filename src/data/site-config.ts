dfa99e9a6b98c3887d06317ede4ebffd2edcdf81
Yorqui <yorquiguzmang@digitechfp.com>
2025-08-06 00:45:36 +0200

    update titulo

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -57,7 +57,7 @@ const siteConfig: SiteConfig = {
             href: '/projects'
         },
         {
-            text: 'Curriculum Vitae',
+            text: 'CV',
             href: '/blog'
         },
         {
@@ -88,13 +88,9 @@ const siteConfig: SiteConfig = {
             text: 'Instagram',
             href: 'https://instagram.com/'
         },
-        {
-            text: 'X/Twitter',
-            href: 'https://twitter.com/'
-        }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
+        title: 'Transformando ideas en software',
         text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
         image: {
             src: '/hero.jpeg',

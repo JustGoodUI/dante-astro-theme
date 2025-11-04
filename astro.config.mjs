8d144c36c7814838ee3341bc2c83fd3c8a7194a3
antonioprieto <antonioprietomontenegro@gmail.com>
2025-11-04 17:37:33 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.website,
+    site: 'https://antonioprieto.github.io'‎,
     vite: {
         plugins: [tailwindcss()]
     },

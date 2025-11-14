a9524b8a56fd62d92877cf3a65498629288f8304
Nadinson <140910047+nrglen@users.noreply.github.com>
2025-11-13 23:54:44 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.website,
+    site: 'https://nrglen.github.io',
     vite: {
         plugins: [tailwindcss()]
     },

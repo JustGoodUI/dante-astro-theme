5202635c301153a2e8dc0d7008947a6edcf5371c
Nadinson <140910047+nrglen@users.noreply.github.com>
2025-11-14 00:16:27 -0500

    igug

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
-import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({

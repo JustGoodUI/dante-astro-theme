6968f7684d8c8221762ac66053ec042eebad3869
McJosebas <mcjosebas@gmail.com>
2026-01-08 08:09:10 +0100

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.website,
+    site: 'https://McJosebas.github.io',
     vite: {
         plugins: [tailwindcss()]
     },

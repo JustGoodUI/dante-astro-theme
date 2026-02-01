d8a53db47bb1857edf37c750d8f2d6fdd4202f49
Damian Alejandro Gey <124899040+DamianCode28@users.noreply.github.com>
2026-02-01 11:14:35 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.website,
+    site: 'https://DamianCode28.github.io',
     vite: {
         plugins: [tailwindcss()]
     },

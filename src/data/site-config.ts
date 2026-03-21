6b538e52b4b01f2c3ead408161eaaaf2bd063e84
Dheiry Yojaira Lopez Santana <79459469+dheirylopez@users.noreply.github.com>
2026-03-21 19:54:41 -0400

    Update site-config.ts
    Actualizando url

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -3,7 +3,7 @@ import hero from '../assets/images/hero.jpg';
 import type { SiteConfig } from '../types';
 
 const siteConfig: SiteConfig = {
-    website: 'https://example.com',
+    website: 'https://dheirylopez.github.io/',
     avatar: {
         src: avatar,
         alt: 'Ethan Donovan'

b29dabd4cead70523b8fb85c320445f5d895f34e
Dheiry Yojaira Lopez Santana <79459469+dheirylopez@users.noreply.github.com>
2026-03-21 21:09:30 -0400

    quite las imagenes de la pagina principal

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -1,13 +1,9 @@
-import hero from 'src/assets/image/hero.jpg';
-import avatar from '../assets/images/avatar.jpg';
+
 import type { SiteConfig } from '../types';
 
 const siteConfig: SiteConfig = {
     website: 'https://dheirylopez.github.io/',
-    avatar: {
-        src: avatar,
-        alt: 'Ethan Donovan'
-    },
+
     title: 'Dante',
     subtitle: 'Minimal Astro.js theme',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
@@ -50,29 +46,29 @@ const siteConfig: SiteConfig = {
             text: 'Github',
             href: 'https://github.com/dheirylopez'
         }
-      
+
     ],
     socialLinks: [
         {
-             text: 'Linkedin',
+            text: 'Linkedin',
             href: 'www.linkedin.com/in/dheiry-lopez-22665aa0'
         },
         {
-           text: 'Github',
+            text: 'Github',
             href: 'https://github.com/dheirylopez'
         },
         {
-          text: 'Upwork',
+            text: 'Upwork',
             href: 'https://www.upwork.com/freelancers/~01d1eb273b35ecee7c?s=1110580753069494272'
         },
         {
             text: 'Instagram',
-            href: 'https://www.instagram.com/lopezdheiry?igsh=YmRjd3NxNmloZW5o&utm_source=qr' 
+            href: 'https://www.instagram.com/lopezdheiry?igsh=YmRjd3NxNmloZW5o&utm_source=qr'
         }
     ],
     hero: {
-  title: 'Hello & Welcome! Explore My Software Projects!',
-  text: `Hi, I’m Dheiry López, a Senior Software Engineer with 7+ years of experience delivering scalable software solutions and supporting cross-functional project execution.
+        title: 'Hello & Welcome! Explore My Software Projects!',
+        text: `Hi, I’m Dheiry López, a Senior Software Engineer with 7+ years of experience delivering scalable software solutions and supporting cross-functional project execution.
 
          My background includes designing and developing enterprise applications, building RESTful APIs, contributing to microservices architectures, supporting AWS-based environments, and developing RPA bots with Automation Anywhere to streamline repetitive business processes and increase operational efficiency.
 
@@ -80,10 +76,7 @@ const siteConfig: SiteConfig = {
 
          Today, I’m focused on helping companies and clients build software and automation solutions that are clean, maintainable, user-centered, and capable of driving long-term impact.`,
 
-        image: {
-            src: hero,
-            alt: 'A person sitting at a desk in front of a computer'
-        },
+
         actions: [
             {
                 text: 'Get in Touch',

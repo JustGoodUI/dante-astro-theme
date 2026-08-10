b807df7688ee208ce0ce31d70c2afd4c4f53bfe0
Juan Martin <jmartinb@unbosque.edu.co>
2026-08-10 12:06:20 -0500

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.website,
+    site: 'https://Gilgamesh-Camilo.github.io',
     vite: {
         plugins: [tailwindcss()]
     },
